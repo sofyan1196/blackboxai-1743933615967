db966a5b74d616e2f58bf59b51e1e000a0ce5537
sofyan1196 <97283666+sofyan1196@users.noreply.github.com>
2025-04-06 17:30:17 +0700

    Add application/config/esim.php 1743935417694

---
+++ application/config/esim.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['esim_api_key'] = 'YOUR_API_KEY_HERE';
+$config['esim_api_url'] = 'https://api.esimaccess.com/v1/';
+$config['esim_callback_url'] = 'YOUR_CALLBACK_URL';
