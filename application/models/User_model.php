59ef5014a976d0cdfa94434e0b32ce709007401f
sofyan1196 <97283666+sofyan1196@users.noreply.github.com>
2025-04-06 17:00:34 +0700

    Add application/models/User_model.php 1743933634689

---
+++ application/models/User_model.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_model extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function login($email, $password) {
+        $this->db->where('email', $email);
+        $query = $this->db->get('users');
+        
+        if ($query->num_rows() == 1) {
+            $user = $query->row();
+            if (password_verify($password, $user->password)) {
+                return $user;
+            }
+        }
+        return false;
+    }
+
+    public function register($data) {
+        return $this->db->insert('users', $data);
+    }
+
+    public function get_user($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('users');
+        return $query->row();
+    }
+
+    public function update_user($id, $data) {
+        $this->db->where('id', $id);
+        return $this->db->update('users', $data);
+    }
+
+    public function delete_user($id) {
+        $this->db->where('id', $id);
+        return $this->db->delete('users');
+    }
+}
