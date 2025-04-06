4baff860bcd9d12384f67874c42617cb9113f528
sofyan1196 <97283666+sofyan1196@users.noreply.github.com>
2025-04-06 17:30:25 +0700

    Add application/config/autoload.php 1743935426211

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -7,4 +7,4 @@ $autoload['drivers'] = array();
 $autoload['helper'] = array('url', 'form', 'file', 'security');
 $autoload['config'] = array();
 $autoload['language'] = array();
-$autoload['model'] = array();
+$autoload['model'] = array('User_model', 'Package_model', 'Order_model', 'Esim_model');
